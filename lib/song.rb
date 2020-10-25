fadfb3db63a62e0b4d4fb9c32cd457f8f71294df
mccray austin <tubach75@hotmail.com>
2020-10-25 15:25:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,7 @@ class Song
     @@songs
   end
 
+=begin
   def self.reset_all
     self.all.clear
   end
@@ -25,6 +26,7 @@ class Song
   def self.count
     self.all.count
   end
+=end
 
   def artist=(artist)
     @artist = artist
